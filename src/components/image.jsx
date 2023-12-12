23cf6d29f0e406f39313c92ae5d7ef6ccb175ae0
Moeez Shahid <moeez.shahid@kwanso.com>
2023-12-12 21:55:49 +0500

    a few images added

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,23 +1,19 @@
 export const Image = ({ title, largeImage, smallImage }) => {
+  console.log("largeImage", largeImage);
   return (
-    <div className='portfolio-item'>
-      <div className='hover-bg'>
-        {' '}
-        <a
-          href={largeImage}
-          title={title}
-          data-lightbox-gallery='gallery1'
-        >
-          <div className='hover-text'>
+    <div className="portfolio-item">
+      <div className="hover-bg">
+        {" "}
+        <a href={largeImage} title={title} data-lightbox-gallery="gallery1">
+          <div className="hover-text">
             <h4>{title}</h4>
           </div>
-          <img
-            src={smallImage}
-            className='img-responsive'
-            alt={title}
-          />{' '}
-        </a>{' '}
+          {largeImage.includes("mp4") ? (
+            <video src={smallImage} className="img-responsive" alt={title} />
+          ) : null}
+          <img src={smallImage} className="img-responsive" alt={title} />{" "}
+        </a>{" "}
       </div>
     </div>
-  )
-}
+  );
+};
